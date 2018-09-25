987b69ea3536fdee7a42d8e76d6643b9ebf6b76c
exberser <exberser@gmail.com>
2018-09-25 12:31:53 +0300

    add css/scss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import test from './test'
+import test from './js/test'
+
+import './style/app.scss';
+// require('./style/app.scss');
 
 let t = test();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+    mode: 'production',
     devtool: "source-map", //для того щоб в консолі зсилалось на основний файл до компіляції
-    entry: './src/index.js', //звідки взяти
+    entry: ['./src/index.js' ], //звідки взяти
     output: { //куди положити
         path: path.join(__dirname, 'build'), //адрес директорії куди ложити
-        filename: 'bundle.js', // назва файлу
+        filename: 'app.js', // назва файлу
         publicPath: '/build/' // публічні файли (стлі, картинки...)
     },
     module: {
@@ -20,7 +22,27 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader'],
+                    }),
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '/css/style.css'
+        })
+    ]
 };
